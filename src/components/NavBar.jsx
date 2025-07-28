405ab031c09e32a0ed6517f091f5dbdc58653afa
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-29 02:50:29 +0500

    navBar working

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,53 @@
+import React, {useEffect} from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { FiShoppingCart } from 'react-icons/fi'
+import { BsChatLeft } from 'react-icons/bs'
+import { RiNotification3Line } from 'react-icons/ri'
+import { MdKeyboardArrowDown } from 'react-icons/md'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
+import avatar from "../data/avatar.jpg"
+import Chat from './Chat'
+import {Notifications} from '.'
+import {Cart} from '.'
+import {UserProfile} from '.'
+import NavButton from './NavButton'
+
+import { useStateContext } from '../contexts/ContextProvider'
+
+const NavBar = () => {
+
+  const {activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick} = useStateContext()
+
+  return (
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      <NavButton title={"Menu"} customFunc={() => setActiveMenu(prev => !prev)} color={"blue"} icon={<AiOutlineMenu />} />
+
+      <div className='flex'>
+        <NavButton title={"Cart"} customFunc={() => handleClick('cart')} color={'blue'} icon={<FiShoppingCart />} />
+        <NavButton title={"Chat"} dotColor='#03C9D7' customFunc={() => handleClick('chat')} color={'blue'} icon={<BsChatLeft />} />
+        <NavButton title={"Notification"} dotColor='#03C9D7' customFunc={() => handleClick('notification')} color={'blue'} icon={<RiNotification3Line />} />
+        <TooltipComponent content={"Profile"} position='BottomCenter'>
+          <div className='flex item-center gap-2 cursor-pointer p-1 hover:bg-gray-100 rounded-lg' onClick={() => handleClick("userProfile")}>
+            <img src={avatar} className='rounded-full w-8 h-8' alt="user profile picture" />
+            <p>
+              <span className='text-gray-400 text-14'>
+                Hi, 
+              </span> {' '}
+              <span className='text-gray-400 font-bold m;-1 text-14'>Mussadiq</span>
+            </p>
+            <MdKeyboardArrowDown className='text-gray-400 text-14 ' />
+          </div>
+        </TooltipComponent>
+
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notifications />}
+        {isClicked.userProfile && <UserProfile />}
+
+      </div>
+
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/NavButton.jsx
@@ -0,0 +1,14 @@
+import { TooltipComponent } from "@syncfusion/ej2-react-popups"
+
+const NavButton = ({title, customFunc, icon, color, dotColor}) => (
+    
+  <TooltipComponent content={title} position='BottomCenter'>
+    <button type='button' onClick={customFunc} style={{color}} className='relative text-xl cursor-pointer rounded-full p-3 hover:bg-gray-100'>
+      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>
+        {icon}
+      </span>
+    </button>
+  </TooltipComponent>
+)
+
+export default NavButton
