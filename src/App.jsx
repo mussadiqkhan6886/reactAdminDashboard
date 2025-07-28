a313d6663155b82cd9435c19461cc5590717950f
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-28 18:33:06 +0500

    done with dep now starting project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FiSettings } from 'react-icons/fi'
+import {TooltipComponent} from "@syncfusion/ej2-react-popups"
 
 const App = () => {
   return (
     <div>
-      
+      <FiSettings />
+      <TooltipComponent />
+      <h1 className='mt-10'>hi</h1>
     </div>
   )
 }
