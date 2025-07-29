8a474fabbb3534b816736be927c0436b095cf185
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-29 18:03:46 +0500

    started with graphs

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,10 @@ const App = () => {
 
   return (
     <div>
-      <Router>
+      <Router future={{
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+  }}>
         <div className='flex relative dark:bg-main-dark-bg'>
           <div className='fixed right-4 bottom-4' style={{zIndex: "1000"}}>
             <TooltipComponent content={"setting"} position='top'>
@@ -38,7 +41,7 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <NavBar />
             </div>
-          </div>
+          
           <div>
             <Routes>
               {/* Dashboard */}
@@ -66,6 +69,7 @@ const App = () => {
               <Route path='/pyramid' element={<Pyramid />} />
               <Route path='/stacked' element={<Stacked />} />
             </Routes>
+          </div>
           </div>
         </div>
       </Router>
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs'
+import { GoDot } from 'react-icons/go';
+import {Stacked, Pie, Button, SparkLine} from "../components"
+import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
+import { useStateContext } from '../contexts/ContextProvider'
+import image from "../data/5124556.jpg"
 
 const Ecommerce = () => {
   return (
-    <div>
-      
+    <div className='mt-12 '>
+      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
+        <div className={`
+            bg-white dark:text-gray-700 dark:bg-secondary-dark-bg 
+            h-44 rounded-xl w-full lg:w-80 p-8 pt-8 m-3 
+            bg-no-repeat bg-cover bg-center
+          `}
+          style={{ backgroundImage: `url(${image})` }}
+        >
+          <div className='flex justify-between items-center'>
+            <div>
+              <p className='font-bold text-gray-600'>Earnings</p>
+              <p className='text-2xl bg-white lg:text-xl'>$63,448.78</p>
+            </div>
+          </div>
+          <div className='mt-6'>
+            <Button color="white" bgColor="blue" text="Download" borderRadius="10px" size={"md"}/>
+          </div>
+        </div>
+        <div className='flex m-3 flex-wrap justify-center gap-1 item-center'>
+          {earningData.map(item => (
+            <div key={item.title} className='bg-white dark:text-gray-700 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl'>
+              <button type='button' style={{color: item.iconColor, backgroundColor: item.iconBg}} className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'>
+                {item.icon}
+              </button>
+              <p className='my-3'>
+                <span className='text-lg font-semibold'>
+                  {item.amount}
+                </span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='flex gap-10 flex-wrap justify-center '>
+          <div className='bg-white dark:text-gray-700 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+            <div className='flex justify-between'>
+              <p className='text-xl font-semibold'>Revenue Updates</p>
+              <div className='flex item-center gap-4'>
+                <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                  <span><GoDot className='bg-black rounded-full text-[7px]' /></span>
+                  <span>Expense</span>
+                </p>
+                <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                  <span><GoDot className='bg-green-400 rounded-full text-[7px]' /></span>
+                  <span>Budget</span>
+                </p>
+              </div>
+            </div>
+            <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+              <div className='border-r-1 border-gray-300 m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>$93,438</span>
+                  <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full bg-green-400 text-white ml-3 text-xs'>23%</span>
+                </p>
+                <p className='text-gray-500 mt-1'>Budget</p>
+              </div>
+              <div className='mt-8'>
+                <p>
+                  <span className='text-3xl font-semibold'>$48,438</span>
+                </p>
+                <p className='text-gray-500 mt-1'>Expense</p>
+              </div>
+              <div className='mt-8' style={{background: "white"}}>
+                <SparkLine currentColor="blue" id="line-sparkline" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+              </div>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
